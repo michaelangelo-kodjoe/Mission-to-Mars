792ab26bdb61ea3eb44730b07e33ec9d9f32ead0
Michaelangelo <Michaelangelo@Gelos-MacBook-Pro.local>
2022-05-01 13:07:38 -0400

    Adding updated files

--- scraping.py
+++ scraping.py
@@ -10,15 +10,21 @@ def scrape_all():
     # Select the path and browser
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=True)
+
+    news_title, news_p = mars_news(browser)
+    hemisphere_image_urls= hemispheres(browser)
+
     # Run all scraping functions and store results in dictionary
     data = {
         "news_title": news_title,
-        "news_paragraph": news_paragraph,
+        "news_paragraph": news_p,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        "hemispheres" : hemisphere_image_urls,
         "last_modified": dt.datetime.now()
+        
     }
-    news_title, news_paragraph = mars_news(browser)
+   
     # End scraping session and return data
     browser.quit()
     return data
@@ -27,7 +33,7 @@ def scrape_all():
 # Mars news scrape function
 def mars_news(browser):
     # Visit the mars nasa news site
-    url = 'https://redplanetscience.com'
+    url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
     browser.visit(url)
     # Optional delay for loading the page
     browser.is_element_present_by_css('div.list_text', wait_time=1)
@@ -46,11 +52,11 @@ def mars_news(browser):
         news_title = slide_elem.find('div', class_='content_title').get_text()
         
         # Use the parent element to find the paragraph text
-        news_paragraph = slide_elem.find('div', class_='article_teaser_body').get_text()
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
     except AttributeError:
         return None, None
         
-    return news_title, news_paragraph
+    return news_title, news_p
 
 ## Featured Image
 # Scrape featured image function
@@ -96,6 +102,32 @@ def mars_facts():
 
     # export the dataframe back to html
     return df.to_html(classes="table table-striped")
+
+# Hemispheres function
+def hemispheres(browser):
+        # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    for e in range(4):
+        #create empty dictionary
+        hemispheres = {}
+        browser.find_by_css('a.product-item h3')[e].click()
+        element = browser.find_link_by_text('Sample').first
+        img_url = element['href']
+        title = browser.find_by_css("h2.title").text
+        hemispheres["img_url"] = img_url
+        hemispheres["title"] = title
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+
+    # 4. Print the list that holds the dictionary of each image url and title.
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
     # If running as script, print scraped data
